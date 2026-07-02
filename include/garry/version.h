49fcd2f26fdcc7f8b0503ca529f8e9e2563ef48a
subagent <dev@local>
2026-07-02 10:38:54 -0500

    feat(version): add version.h public header for chain iteration

---
+++ include/garry/version.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026 Matthew Hughes (Argylelabcoat)
+ * Licensed under the BSD-3-Clause License.
+ * See LICENSE file for details.
+ */
+
+#ifndef GARRY_VERSION_H
+#define GARRY_VERSION_H
+
+#include "garry/export.h"
+#include "garry/types.h"
+
+#ifndef GARRY_DATABASE_FWD_DEFINED
+#define GARRY_DATABASE_FWD_DEFINED
+typedef struct garry_database garry_database;
+#endif
+
+typedef struct garry_version_iter garry_version_iter;
+
+GARRY_API garry_version_iter *garry_version_iter_open(
+    garry_database *db, garry_txn txn,
+    const garry_u8 *key, garry_i32 klen);
+
+GARRY_API garry_bool garry_version_iter_next(
+    garry_version_iter *iter,
+    garry_i32 *txid,
+    garry_u8 **value,
+    garry_i32 *vlen,
+    garry_bool *is_tombstone);
+
+GARRY_API void garry_version_iter_close(garry_version_iter *iter);
+
+#endif /* GARRY_VERSION_H */
